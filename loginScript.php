330b8a9884722f6af2d74ebeb673fc06c89fa5f2
MartinTrappenburg <martintrap1@gmail.com>
2021-06-25 14:35:35 +0200

    commit, working on login

--- loginScript.php
+++ loginScript.php
@@ -1,4 +1,8 @@
 <?php
 	#code hier
+    require_once("./connect.php");
+    require("./functions.php");
+    $email = sanitize($_POST["email"]);
+    $password = sanitize($_POST["password"]);
     var_dump($_POST);
 ?>
